c715d2fa7650824ddee5311a77e5c74f5b6f80c5
Rodrigo Rocha <rodrigo.desenvolvedor.web@gmail.com>
2020-04-20 14:14:00 -0300

    Exportation setStore

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import { appFactory } from './src/app'
 import { setScope, createComponent, logComponent, html, css } from './src/component'
 import { routerFactory } from './src/router'
-import { getState, watch, mapActions, logState } from './src/store'
+import { getState, watch, mapActions, logState, setStore } from './src/store'
 
 
 
@@ -16,6 +16,7 @@ export { html }
 export { css }
 
 //store modules
+export { setStore }
 export { getState }
 export { watch }
 export { mapActions }
